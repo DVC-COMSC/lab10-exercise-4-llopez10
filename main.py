e318a4703b61fde9c32f0f6a31b68b1783a8a140
llopezescobar <126624819+llopez10@users.noreply.github.com>
2023-03-09 22:55:36 +0000

    completed exercise 4

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
-    pass
+  celsius = float(input())
+  Fahrenheit = ((9/5)*celsius)+32
+  print (f'fahrenheit: {Fahrenheit}')
 
+ 
 
 if __name__ == '__main__':
     main()
